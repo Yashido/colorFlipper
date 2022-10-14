059df1cc3a1bbad5f74973de32f13ddd8697b1ec
Yashido <jbpablico27@gmail.com>
2022-10-14 05:56:09 -0500

    Created some css and began javascript logic

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function randomNumber() {
     const rndNum = Math.floor(Math.random()*2);
 };
+
+const btnColor =document.getElementById('btn--color');
+let bgColor = document.body.style.backgroundColor()
+
+function colorArray() {
+    let clrArray = ['red','blue', 'green', 'purple'];
+    let i = 0;
+    document.body.style.background.color('clrArray[i]')
+    i++;
+}
