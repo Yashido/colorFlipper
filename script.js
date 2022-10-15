7ef8b1bf6753a15e644845b2867954a693d9f3ad
wimllie <jbpablico27@gmail.com>
2022-10-15 06:28:37 -0500

    Finished creating the color randomizer in JS

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ function randomNumber() {
     const rndNum = Math.floor(Math.random()*2);
 };
 
-const btnColor =document.getElementById('btn--color');
 var i = 0;
+let clrArray = ['red', 'orange', 'yellow','green','cyan','azure','blue','violet','magenta','rose'];
+
 
 function colorArray() {
-    let clrArray = ['red','blue', 'green', 'purple'];
     document.getElementById('page--container').style.backgroundColor = clrArray[i];
     i++;
-    console.log(i);
+        if(i >= 9) {
+            i = 0;
+        };
     return;
 }
