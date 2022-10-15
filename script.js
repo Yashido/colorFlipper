d73fc9890a2201f5aff7b36e861c9d33234dfa4a
Yashido <jbpablico27@gmail.com>
2022-10-15 07:33:26 -0500

    Completed the logic for Hex

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function randomNumber() {
-    const rndNum = Math.floor(Math.random()*2);
+    const rndNum = Math.floor(Math.random()* (16 - 0) + 0);
+    return rndNum;
 };
-
+var hexclr =[]
+var x = 0;
 var i = 0;
 let clrArray = ['red', 'orange', 'yellow','green','cyan','azure','blue','violet','magenta','rose'];
-
+let hxArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
 
 function colorArray() {
     document.getElementById('page--container').style.backgroundColor = clrArray[i];
@@ -13,4 +15,19 @@ function colorArray() {
             i = 0;
         };
     return;
+}
+
+function  hexArray() {
+    while(x <= 5) {
+        hexclr.push(hxArray[randomNumber()]);
+        console.log(hexclr);
+        x++;
+    }
+    let hex = hexclr.join('');
+    document.getElementById('page--container').style.backgroundColor = '#'+ hex;
+    console.log(hex);
+    if(hexclr.length >=6) {
+        hexclr = [];
+        x = 0;
+    }
 }
