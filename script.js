c5744c7f44f8f5a12d2a257edfef3f24850fd14f
wimllie <jbpablico27@gmail.com>
2022-10-16 02:19:31 -0500

    Added DOM update in javascript to display color and hex

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ function randomNumber() {
     const rndNum = Math.floor(Math.random()* (16 - 0) + 0);
     return rndNum;
 };
+
+const color = document.getElementById('color');
 var hexclr =[]
 var x = 0;
 var i = 0;
-let clrArray = ['red', 'orange', 'yellow','green','cyan','azure','blue','violet','magenta','rose'];
+let clrArray = ['Red', 'orange', 'yellow','green','cyan','azure','blue','violet','magenta','rose'];
 let hxArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
 
 function colorArray() {
     document.getElementById('page--container').style.backgroundColor = clrArray[i];
+    color.innerHTML = clrArray[i];
     i++;
         if(i >= 9) {
             i = 0;
@@ -25,6 +28,7 @@ function  hexArray() {
     }
     let hex = hexclr.join('');
     document.getElementById('page--container').style.backgroundColor = '#'+ hex;
+    color.innerHTML = '#' + hex;
     console.log(hex);
     if(hexclr.length >=6) {
         hexclr = [];
